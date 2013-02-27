07e4ec2a07b00feead5b640c1d66989b9bdba23f
luzhuomi <luzhuomi@fefd3a9c-8ca3-22ac-ec9a-160a77092924>
2013-02-27 01:29:02 +0000

    modified run_test.sh

--- benchmarks/run_test.sh
+++ benchmarks/run_test.sh
@@ -2,9 +2,12 @@
 
 
 # case 8
+rm results/DERIV8*.tsv
 ./Timer ./DERIV "^((A)|(AB)|(B))*$" results/DERIV8a.tsv data/a1 data/a2 data/a3 data/a4 data/a5 data/a6 data/a7 data/a8 data/a9 data/a10 
 ./Timer ./DERIV "^((A)|(AB)|(B))*$" results/DERIV8ab.tsv data/ab1 data/ab2 data/ab3 data/ab4 data/ab5 data/ab6 data/ab7 data/ab8 data/ab9 data/ab10 
-./Timer ./DERIV "^((A)|(AB)|(B))*$" results/DERIV8a.tsv data/b1 data/b2 data/b3 data/b4 data/b5 data/b6 data/b7 data/b8 data/b9 data/b10 
+./Timer ./DERIV "^((A)|(AB)|(B))*$" results/DERIV8b.tsv data/b1 data/b2 data/b3 data/b4 data/b5 data/b6 data/b7 data/b8 data/b9 data/b10 
 
-
+rm results/TDFA8*.tsv
 ./Timer ./TDFA "^((A)|(AB)|(B))*$" results/TDFA8a.tsv data/a1 data/a2 data/a3 data/a4 data/a5 data/a6 data/a7 data/a8 data/a9 data/a10 
+./Timer ./TDFA "^((A)|(AB)|(B))*$" results/TDFA8ab.tsv data/ab1 data/ab2 data/ab3 data/ab4 data/ab5 data/ab6 data/ab7 data/ab8 data/ab9 data/ab10
+./Timer ./TDFA "^((A)|(AB)|(B))*$" results/TDFA8b.tsv data/b1 data/b2 data/b3 data/b4 data/b5 data/b6 data/b7 data/b8 data/b9 data/b10
